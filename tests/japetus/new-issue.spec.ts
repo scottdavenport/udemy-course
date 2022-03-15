23e57979300e868dfbae07e2e5080c08e6934ad8
scottdavenport <scott@pinpoint.com>
2022-03-15 13:46:57 -0400

    working on new issue

--- tests/japetus/new-issue.spec.ts
+++ tests/japetus/new-issue.spec.ts
@@ -1,23 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Create Issue', () => {
-  const baseUrl = 'https://app.japet.us'
+test.describe.only('Create Issue', () => {
+  const loginUrl =
+    'https://local.japet.us:3001/login-test-automation?token=12345-098877-910'
+  const logoutUrl = 'https://local.japet.us:3001/logout'
 
-  test.beforeEach(async () => {
+  test.beforeEach(async ({ page }) => {
     // Authenticate
+    await page.goto(loginUrl)
   })
 
   test('Create Issue', async ({ page }) => {
-    await page.goto(baseUrl)
     // During initial auth clicking "Go to My Issues" is required
     await page.click('text="Go to My Issues"')
 
     // Create new issue and fill in form
-    await page.click('text="New Issue"')
-    await page.type('input[placeholder="Issue title"]', 'Test Issue')
-    await page.type('text="Add a description..."', 'Test Issue Description')
+    await page.click('.pw-new-issue')
+    await page.click('.pw-select-project')
+    await page.click('.pw-combo-select.pw-10100')
+    await page.click('.pw-select-issue-type')
+    await page.click('.pw-combo-select.pw-10203')
+    await page.click('.pw-select-issue-priority')
+    await page.click('pw-combo-select.pw-2')
+    await page.pause()
+  })
 
-    // Select Project, Type, Priority, and Assignee from dropdown
-    await page.selectOption('text="Select a project..."', 'text="Japetus"')
+  test.afterEach(async ({ page }) => {
+    await page.goto(logoutUrl)
   })
 })
